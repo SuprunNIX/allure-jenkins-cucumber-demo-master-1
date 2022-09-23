d959f60fb7e4f948e47cec1d2d0baa47ea0e7d27
SuprunNIX <rostislav.suprun@nixsolutions.com>
2022-09-23 17:20:56 +0300

    set valid credential

--- src/test/java/by/peshkur/allure/pages/SignInPage.java
+++ src/test/java/by/peshkur/allure/pages/SignInPage.java
@@ -6,8 +6,8 @@ import org.openqa.selenium.By;
 import static com.codeborne.selenide.Selenide.$;
 
 public class SignInPage {
-    private SelenideElement loginInput = $(By.cssSelector("#email1456546"));
-    private SelenideElement passwordInput = $(By.cssSelector("#password12415"));
+    private SelenideElement loginInput = $(By.cssSelector("#email"));
+    private SelenideElement passwordInput = $(By.cssSelector("#password"));
 
     public void inputLogin(String text) {
         this.loginInput.val(text);
