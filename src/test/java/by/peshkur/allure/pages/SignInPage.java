3141007a317f69d74cf7852d38c355355a10aecb
SuprunNIX <rostislav.suprun@nixsolutions.com>
2022-09-23 17:14:41 +0300

    added futures of 'add screenshot after fail test'
    change email and password - test will be fail

--- src/test/java/by/peshkur/allure/pages/SignInPage.java
+++ src/test/java/by/peshkur/allure/pages/SignInPage.java
@@ -6,8 +6,8 @@ import org.openqa.selenium.By;
 import static com.codeborne.selenide.Selenide.$;
 
 public class SignInPage {
-    private SelenideElement loginInput = $(By.cssSelector("#email14565465"));
-    private SelenideElement passwordInput = $(By.cssSelector("#password124515"));
+    private SelenideElement loginInput = $(By.cssSelector("#email1456546"));
+    private SelenideElement passwordInput = $(By.cssSelector("#password12415"));
 
     public void inputLogin(String text) {
         this.loginInput.val(text);
