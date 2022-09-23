08808d6691e16a828d8729b44392e6b91799b614
SuprunNIX <rostislav.suprun@nixsolutions.com>
2022-09-23 17:14:03 +0300

    added futures of 'add screenshot after fail test'
    change email and password - test will be fail

--- src/test/java/by/peshkur/allure/pages/SignInPage.java
+++ src/test/java/by/peshkur/allure/pages/SignInPage.java
@@ -6,8 +6,8 @@ import org.openqa.selenium.By;
 import static com.codeborne.selenide.Selenide.$;
 
 public class SignInPage {
-    private SelenideElement loginInput = $(By.cssSelector("#email"));
-    private SelenideElement passwordInput = $(By.cssSelector("#password"));
+    private SelenideElement loginInput = $(By.cssSelector("#email14565465"));
+    private SelenideElement passwordInput = $(By.cssSelector("#password124515"));
 
     public void inputLogin(String text) {
         this.loginInput.val(text);
--- src/test/java/by/peshkur/allure/steps/Hooks.java
+++ src/test/java/by/peshkur/allure/steps/Hooks.java
@@ -1,13 +1,36 @@
 package by.peshkur.allure.steps;
 
+import by.peshkur.allure.pages.SignInPage;
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import io.qameta.allure.Allure;
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
-import static com.codeborne.selenide.Selenide.open;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import static com.codeborne.selenide.Selenide.*;
 
 public class Hooks {
 
     @Before
     public void openUrl() {
         open("https://grinfer.com/");
+    }
+
+    @Attachment(value = "Page screenshot", type = "image/png")
+    public  byte[] saveScreenshot(byte[] screenshot) {
+        return screenshot;
+    }
+
+    @After
+    public void af0(Scenario scenario) throws IllegalMonitorStateException {
+        scenario.log("After Hook");
+        if(scenario.isFailed()) {
+            Allure.addAttachment("Any Name", new ByteArrayInputStream(((TakesScreenshot)webdriver()).getScreenshotAs(OutputType.BYTES)));
+        }
     }
 }
